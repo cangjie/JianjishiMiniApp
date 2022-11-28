f7eb541a732ffda977987463363ea4b39bdbbf36
cangjie <cangjie@hotmail.com>
2022-11-28 23:17:49 +0800

    where

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -114,8 +114,10 @@ namespace MiniApp.Controllers
                 return BadRequest();
             }
             var reserveList = await _context.reserve
-                .Join(_context.timeTable, r => r.time_table_id, t => t.id, (r, t) => new { r.open_id, t.shop_name, r.time_table_description, r.reserve_date })
+                .Join(_context.timeTable, r => r.time_table_id, t => t.id, (r, t) => new { r.open_id, t.shop_name, r.time_table_description, r.reserve_date, t.shop_id })
+                .Where(t => (shopId == 0 || t.shop_id == shopId))
                 .Join(_context.miniUser, tt => tt.open_id, u => u.open_id, (tt, u) => new { u.real_name, u.cell_number, tt.shop_name, tt.time_table_description, tt.reserve_date })
+                
                 .ToListAsync();
             return Ok(reserveList);
         }
