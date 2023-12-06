2a641195a282709cc08e57b3644d6fde6645453b
苍杰 <cangjie@hotmail.com>
2023-12-06 22:35:24 +0800

    set status

--- Models/Reserve.cs
+++ Models/Reserve.cs
@@ -21,11 +21,64 @@ namespace MiniApp.Models
 		
 		public string product_name {get; set;} = "";
 		public int order_id {get; set;}
+
+		[NotMapped]
+		public DateTime? startTime
+		{
+			get
+			{
+				try
+				{
+					return DateTime.Parse(time_table_description.Split('-')[0].Trim());
+				}
+				catch
+				{
+					return null;
+				}
+			}
+		}
+
 		[NotMapped]
 		public OrderOnline? order {get; set;} = null;
 		public string shop_name { get; set; } = "";
 		[NotMapped]
 		public bool valid {get; set;} = true;
+		[NotMapped]
+		public string status
+		{
+			get
+			{
+				string s = "待支付";
+				if (order_id != 0 && order != null)
+				{
+					if (order.pay_state == 1)
+					{
+						s = "已支付";
+						if (order.refunds != null && order.refunds.Length > 0)
+						{
+							s = "已退款";
+						}
+					}
+					if (s.Trim().Equals("已支付"))
+					{
+						if (reserve_date.Date == DateTime.Now.Date)
+						{
+							s = "即将开始";
+							if (startTime <= DateTime.Now)
+							{
+								s = "已过时";
+							}
+						}
+						else
+						{
+							s = "已预约";
+						}
+					}
+
+				}
+				return s.Trim();
+			}
+		}
 
 
 	}
