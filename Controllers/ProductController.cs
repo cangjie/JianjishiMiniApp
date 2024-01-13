1859b8b0790f3392551973fe36613e7cede8c102
苍杰 <cangjie@hotmail.com>
2024-01-13 19:39:41 +0800

    place order

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MiniApp.Models;
+using MiniApp.Models.Order;
 
 namespace MiniApp.Controllers
 {
@@ -11,11 +12,49 @@ namespace MiniApp.Controllers
 	{
         private readonly SqlServerContext _context;
         private readonly IConfiguration _config;
+        private readonly MiniUserController _userHelper;
+        private readonly OrderController _orderHelper;
 
         public ProductController(SqlServerContext context, IConfiguration config)
 		{
             _context = context;
             _config = config;
+            _userHelper = new MiniUserController(context, config);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<OrderOnline>> PlaceOrder(int productId, string sessionKey)
+        {
+            Product p = await _context.product.FindAsync(productId);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            sessionKey = Util.UrlDecode(sessionKey.Trim());
+            MiniUser user = (MiniUser)((OkObjectResult)(await _userHelper.GetBySessionKey(sessionKey)).Result).Value;
+            OrderOnline order = new OrderOnline()
+            {
+                open_id = user.open_id.Trim(),
+                shop = "",
+                order_price = p.sale_price,
+                order_real_pay_price = p.sale_price,
+                final_price = p.sale_price,
+                staff_open_id = "",
+                type = ""
+            };
+            await _context.OrderOnline.AddAsync(order);
+            await _context.SaveChangesAsync();
+            OrderPayment payment = new OrderPayment()
+            {
+                order_id = order.id,
+                mch_id = 1,
+                amount = order.final_price,
+                open_id = user.open_id,
+                pay_method = "微信支付"
+            };
+            await _context.orderPayment.AddAsync(payment);
+            await _context.SaveChangesAsync();
+            return await _orderHelper.GetWholeOrder(order.id);
         }
 
         [HttpGet]
